53c976a157dab46e04c7b1449a75c0537065bc7c
morgan <morgan@dunamu.com>
2021-12-28 20:50:44 +0900

    polish build.gradle.kts

---
+++ aggregator-service/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot")
+
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    tasks {
+        getByName<BootJar>("bootJar") {
+            enabled = false
+        }
+
+        getByName<Jar>("jar") {
+            enabled = true
+        }
+
+        test {
+            useJUnitPlatform()
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,67 +1,50 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    idea
-    id("org.springframework.boot") version "2.6.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-
+    id("org.springframework.boot") version "2.6.2" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.jpa") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10" apply false
+    kotlin("plugin.jpa") version "1.6.10" apply false
 }
 
-val bootJar: BootJar by tasks
-
-bootJar.enabled = false
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 allprojects {
+    group = "com.sample"
+    version = "0.0.1-SNAPSHOT"
+
     repositories {
         mavenCentral()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
     }
 }
 
 subprojects {
-    group = "com.sample"
-    version = "0.0.1-SNAPSHOT"
+    repositories {
+        mavenCentral()
+    }
 
-    apply(plugin = "idea")
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-kapt")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
+    apply {
+        plugin("idea")
+        plugin("kotlin")
+        plugin("kotlin-kapt")
+        plugin("io.spring.dependency-management")
+    }
 
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation(kotlin("stdlib"))
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
         implementation("io.github.microutils:kotlin-logging:2.1.21")
         implementation("ch.qos.logback:logback-classic:1.2.9")
 
         runtimeOnly("com.h2database:h2")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-    }
-
-    tasks.getByName<BootJar>("bootJar") {
-        enabled = false
-    }
-
-    tasks.getByName<Jar>("jar") {
-        enabled = true
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
-        }
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
     }
 }
