764e786d1ee1e5b353c8a1c744466d5d10a0a963
morgan <morgan@dunamu.com>
2021-12-28 20:17:33 +0900

    setting up dependency for grpc/proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,67 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
+    idea
     id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
 }
 
-group = "com.sample"
-version = "0.0.1-SNAPSHOT"
+val bootJar: BootJar by tasks
+
+bootJar.enabled = false
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+allprojects {
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects {
+    group = "com.sample"
+    version = "0.0.1-SNAPSHOT"
+
+    apply(plugin = "idea")
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-kapt")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        implementation("io.github.microutils:kotlin-logging:2.1.21")
+        implementation("ch.qos.logback:logback-classic:1.2.9")
+
+        runtimeOnly("com.h2database:h2")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.getByName<BootJar>("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName<Jar>("jar") {
+        enabled = true
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
---
+++ proto/build.gradle.kts
@@ -0,0 +1,51 @@
+import com.google.protobuf.gradle.*
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+            mavenCentral()
+        }
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+    }
+}
+
+plugins {
+    id("com.google.protobuf") version "0.8.18"
+}
+
+dependencies {
+    implementation("io.grpc:grpc-netty-shaded:1.43.0")
+    implementation("io.grpc:grpc-protobuf:1.42.1")
+    implementation("io.grpc:grpc-stub:1.43.0")
+
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.0.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.42.1"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
+
+idea {
+    module {
+        generatedSourceDirs.add(file("${protobuf.protobuf.generatedFilesBaseDir}/main/grpc"))
+    }
+}
